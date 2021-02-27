d7e5d2e4fdf7de08989d04e0db467fa11a53db8f
davidebbitt <david.ebbitt@gmail.com>
2021-02-27 11:13:12 +0000

    Recipe Detail displaying

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+
+  recipes: Recipe[] = [
+    {
+      id: "r1",
+      title: "Vegan Steak Bake",
+      imageUrl:
+      "https://www.exceedinglyvegan.com/vegan/thumbnails/thumbnailimage/5116/0284/4165/vegan-steak-bake-recipe-greggs.jpg",
+      ingredients: ['Tofu', 'Red Onion, Walnuts']
+    },
+    {
+
+      id: "r2",
+      title: "Potato Gratin",
+      imageUrl:
+      "https://www.exceedinglyvegan.com/vegan/thumbnails/thumbnailimage/5116/0284/4165/vegan-steak-bake-recipe-greggs.jpg",
+      ingredients: ['Potato', 'Onion, Sunflower Seeds']
+    }
+  ]
+
+  constructor() { }
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return {...this.recipes.find(recipe => {
+      return recipe.id === recipeId;
+    })};
+  }
+}
