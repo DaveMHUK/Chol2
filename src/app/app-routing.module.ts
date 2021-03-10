3678a473258ca9f682b90a14ea44a38ce019be9b
davidebbitt <david.ebbitt@gmail.com>
2021-03-10 13:54:24 +0000

    Reverted to pre-tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,49 +2,22 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'recipes',
-    pathMatch: 'full'
-  },
-  {
-    path: 'recipes',
+  { path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule) },
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  { path: 'recipes',
     children: [
-      {
-        path: '',
-        loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
-      },
-      {
-        path: ':recipeId',
-        loadChildren: () => import('./recipes/recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule)
-      }
-    ]
-  },
-  {
-    path: 'results',
-    loadChildren: () => import('./results/results.module').then( m => m.ResultsPageModule)
-  },
-  {
-    path: 'testresult',
-    loadChildren: () => import('./testresult/testresult.module').then( m => m.TestresultPageModule)
-  },
-  {
-    path: 'testresults',
+      { path: '', loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)},
+      { path: ':recipeId', loadChildren: () => import('./recipes/recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule) }
+    ]},
+  { path: 'results', loadChildren: () => import('./results/results.module').then( m => m.ResultsPageModule) },
+  { path: 'testresult',
+    loadChildren: () => import('./testresult/testresult.module').then( m => m.TestresultPageModule) },
+  { path: 'testresults',
     children: [
-      {
-        path: '',
-        loadChildren: () => import('./testresults/testresults.module').then( m => m.TestresultsPageModule)
-      },
-      {
-        path: ':testresultId',
-        loadChildren: () => import('./testresults/testresult-detail/testresult-detail.module').then( m => m.TestresultDetailPageModule)
-      }
-    ]
-  },
+      { path: '', loadChildren: () => import('./testresults/testresults.module').then( m => m.TestresultsPageModule) },
+      { path: ':testresultId', loadChildren: () => import('./testresults/testresult-detail/testresult-detail.module').then( m => m.TestresultDetailPageModule) }
+    ]},
 ];
 
 @NgModule({
